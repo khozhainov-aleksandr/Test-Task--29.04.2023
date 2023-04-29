090e20b9538886df786f992675f3f80340182bf9
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-04-29 21:45:06 +0300

    add page: coming, groups, products, users, setting

---
+++ src/pages/groups.tsx
@@ -0,0 +1,18 @@
+import TopMenu from '@/components/TopMenu/TopMenu';
+import NavigationMenu from '@/components/NavigationMenu/NavigationMenu';
+
+import styles from '@/styles/Home.module.css'
+
+export default function Groups() {
+  return (
+    <>
+      <header className={styles.header}>
+        <TopMenu />
+      </header>
+      <main className={styles.main}>
+        <NavigationMenu />
+        Groups
+      </main>
+    </>
+  )
+}
