579a44bfa3e90dcc5238fa85c1eea9d29ca07857
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-04-29 21:45:43 +0300

    add Navigation component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import TopMenu from '../components/TopMenu/TopMenu';
+import TopMenu from '@/components/TopMenu/TopMenu';
+import NavigationMenu from '@/components/NavigationMenu/NavigationMenu';
+
 import styles from '@/styles/Home.module.css'
 
 export default function Home() {
@@ -8,6 +10,7 @@ export default function Home() {
         <TopMenu />
       </header>
       <main className={styles.main}>
+        <NavigationMenu />
         Content !!!
       </main>
     </>
