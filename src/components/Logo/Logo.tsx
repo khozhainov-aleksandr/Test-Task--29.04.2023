672f2540c4bfae503fffb80958ad490983ecbba0
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-04-29 21:42:15 +0300

    update Logo and add link to home

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 import ShieldIcon from '../../icon/shield.svg';
@@ -5,13 +6,13 @@ import styles from './Logo.module.css'
 
 export default function Logo() {
   return (
-    <div className={styles.logo}>
+    <Link className={styles.logo} href="/">
       <Image
         className={styles.mainLogo}
         src={ShieldIcon}
         alt="Main logo, Green shield"
       />
       <h3 className={styles.logoTitle}>Inventory</h3>
-    </div>
+    </Link>
   )
 }
