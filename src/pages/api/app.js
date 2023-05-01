d9eab480b535a529bfac3ac159b426fd49e691bb
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-01 23:00:23 +0300

    add more obj in API

--- src/pages/api/app.js
+++ src/pages/api/app.js
@@ -1,28 +1,28 @@
-const orders = [
+export const orders = [
   {
     id: 1,
     title: 'Order 1',
     date: '2017-06-29 12:09:33',
     description: 'desc',
-    get products () { return products }
+    get products() { return products },
   },
   {
     id: 2,
     title: 'Order 2',
-    date: '2017-06-29 12:09:33',
+    date: '2018-06-29 05:09:33',
     description: 'desc',
-    get products () { return products }
+    get products() { return products },
   },
   {
     id: 3,
     title: 'Order 3',
-    date: '2017-06-29 12:09:33',
+    date: '2019-06-29 10:09:33',
     description: 'desc',
-    get products () { return products },
-  }
+    get products() { return products },
+  },
 ]
 
-const products = [
+export const products = [
   {
     id: 1,
     serialNumber: 1234,
@@ -44,6 +44,120 @@ const products = [
   },
   {
     id: 2,
+    serialNumber: 1234,
+    isNew: 1,
+    photo: 'pathToFile.jpg',
+    title: 'Product 1',
+    type: 'Monitors',
+    specification: 'Specification 1',
+    guarantee: {
+      start: '2017-06-29 12:09:33',
+      end: '2017-06-29 12:09:33'
+    },
+    price: [
+      {value: 100, symbol: 'USD', isDefault: 0},
+      {value: 2600, symbol: 'UAH', isDefault: 1}
+    ],
+    order: 1,
+    date: '2017-06-29 12:09:33'
+  },
+  {
+    id: 3,
+    serialNumber: 1234,
+    isNew: 1,
+    photo: 'pathToFile.jpg',
+    title: 'Product 1',
+    type: 'Monitors',
+    specification: 'Specification 1',
+    guarantee: {
+      start: '2017-06-29 12:09:33',
+      end: '2017-06-29 12:09:33'
+    },
+    price: [
+      {value: 100, symbol: 'USD', isDefault: 0},
+      {value: 2600, symbol: 'UAH', isDefault: 1}
+    ],
+    order: 1,
+    date: '2017-06-29 12:09:33'
+  },
+  {
+    id: 4,
+    serialNumber: 1234,
+    isNew: 1,
+    photo: 'pathToFile.jpg',
+    title: 'Product 1',
+    type: 'Monitors',
+    specification: 'Specification 1',
+    guarantee: {
+      start: '2017-06-29 12:09:33',
+      end: '2017-06-29 12:09:33'
+    },
+    price: [
+      {value: 100, symbol: 'USD', isDefault: 0},
+      {value: 2600, symbol: 'UAH', isDefault: 1}
+    ],
+    order: 1,
+    date: '2017-06-29 12:09:33'
+  },
+  {
+    id: 5,
+    serialNumber: 1234,
+    isNew: 1,
+    photo: 'pathToFile.jpg',
+    title: 'Product 1',
+    type: 'Monitors',
+    specification: 'Specification 1',
+    guarantee: {
+      start: '2017-06-29 12:09:33',
+      end: '2017-06-29 12:09:33'
+    },
+    price: [
+      {value: 100, symbol: 'USD', isDefault: 0},
+      {value: 2600, symbol: 'UAH', isDefault: 1}
+    ],
+    order: 2,
+    date: '2017-06-29 12:09:33'
+  },
+  {
+    id: 6,
+    serialNumber: 1234,
+    isNew: 1,
+    photo: 'pathToFile.jpg',
+    title: 'Product 1',
+    type: 'Monitors',
+    specification: 'Specification 1',
+    guarantee: {
+      start: '2017-06-29 12:09:33',
+      end: '2017-06-29 12:09:33'
+    },
+    price: [
+      {value: 100, symbol: 'USD', isDefault: 0},
+      {value: 2600, symbol: 'UAH', isDefault: 1}
+    ],
+    order: 2,
+    date: '2017-06-29 12:09:33'
+  },
+  {
+    id: 7,
+    serialNumber: 1234,
+    isNew: 1,
+    photo: 'pathToFile.jpg',
+    title: 'Product 1',
+    type: 'Monitors',
+    specification: 'Specification 1',
+    guarantee: {
+      start: '2017-06-29 12:09:33',
+      end: '2017-06-29 12:09:33'
+    },
+    price: [
+      {value: 100, symbol: 'USD', isDefault: 0},
+      {value: 2600, symbol: 'UAH', isDefault: 1}
+    ],
+    order: 2,
+    date: '2017-06-29 12:09:33'
+  },
+  {
+    id: 8,
     serialNumber: 1234,
     isNew: 1,
     photo: 'pathToFile.jpg',
