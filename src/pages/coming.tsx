5ddfe43add33aecc40fceae2db448182739b6b91
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-04 21:31:05 +0300

    update and finish coming page

--- src/pages/coming.tsx
+++ src/pages/coming.tsx
@@ -1,9 +1,31 @@
+import { products } from '@/pages/api/app';
+import PageTitleQuantity from '@/components/PageTitleQuantity/PageTitleQuantity';
+import Products from '@/components/Products/Products'
+
 import styles from '@/styles/Home.module.css'
+
+interface Product {
+  id: number;
+  serialNumber: number;
+  isNew: number;
+  photo: string;
+  title: string;
+  type: string;
+  specification: string;
+  guarantee: { start: string; end: string; }
+  price: { value: number; symbol: string; isDefault: number; }[]
+  order: number;
+  date: string;
+}
 
 export default function Coming() {
   return (
     <main className={styles.main}>
-      <p>Coming</p>
+      <PageTitleQuantity
+        title='Приходы'
+        quantity={products.length}
+      />
+      <Products />
     </main>
   )
 }
