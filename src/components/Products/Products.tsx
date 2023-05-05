917db36fba578fcbc58ee61ee2ff055775f7a755
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-05 11:00:22 +0300

    add second chose

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -9,7 +9,8 @@ import deleteIcon from '@/icon/delete.svg';
 import styles from './Products.module.css';
 
 interface Props {
-  prodList: Product[]
+  prodList: Product[];
+  fullProductsList: boolean;
 };
 
 interface Product {
@@ -26,7 +27,7 @@ interface Product {
   date: string;
 }
 
-const Products = ({ prodList = products }: Props) => {
+const Products = ({ prodList = products, fullProductsList = false }: Props) => {
   const [popUp, setPopUp] = useState<boolean>(false);
   const [deleteEl, setDeleteEl] = useState<Product>();
 
@@ -42,56 +43,129 @@ const Products = ({ prodList = products }: Props) => {
 
   return (
     <>
-      {prodList.map((prod: Product) => {
-        const {
-          id,
-          isNew,
-          title,
-          type,
-          specification,
-          guarantee,
-          serialNumber,
-          price
-        } = prod;
+      {fullProductsList ? (
+        <>
+          {prodList.map((prod: Product) => {
+            const {
+              id,
+              isNew,
+              title,
+              type,
+              specification,
+              guarantee,
+              serialNumber,
+              price,
+            } = prod;
 
-        return (
-          <div key={id} className={styles.productBox}>
-            <span className={
-              isNew ? [styles.statusBePresent] : [styles.statusAbsent]
-            } />
-            <Image
-              className={styles.photo}
-              src={photoProdIcon}
-              alt="Photo product icon"
-            />
-            <div className={styles.typeWrapper}>
-              <p className={styles.type}>{type}</p>
-              <h6 className={styles.title}>{title}</h6>
-            </div>
-            <div className={styles.descWrapper}>
-              <p className={styles.descSpec}>{specification}</p>
-              <p className={styles.descGuar}>Guarantee: {guarantee.end}</p>
-            </div>
-            <div className={styles.priceWrapper}>
-              <p className={styles.price}>Price:
-                <span>
-                  {price.map((p) => p.isDefault ? (` ${p.value} ${p.symbol}`) : '')}
-                </span>
-              </p>
-              <p className={styles.number}>Serial Number: {serialNumber}</p>
-            </div>
-            <Image
-              className={styles.delete}
-              src={deleteIcon}
-              alt="Delete icon"
-              onClick={() => {
-                getCurrentElement(id);
-                openPopUp();
-              }}
-            />
-          </div>
-        )
-      })}
+            const curseUSD: number = 28;
+            let totalPrice: number = 0;
+
+            price.forEach((el) => {
+              el.symbol === 'UAH'
+                ? (totalPrice += el.value)
+                : (totalPrice += el.value * curseUSD)
+            });
+
+            return (
+              <div key={id} className={styles.productBox}>
+                <span className={
+                  isNew ? [styles.statusBePresent] : [styles.statusAbsent]
+                } />
+                <Image
+                  className={styles.photo}
+                  src={photoProdIcon}
+                  alt="Photo product icon"
+                />
+                <div className={styles.typeWrapper}>
+                  <p className={styles.type}>{type}</p>
+                  <h6 className={styles.title}>{title}</h6>
+                </div>
+                <div className={styles.descWrapper}>
+                  <p className={styles.descSpec}>{specification}</p>
+                  <p className={styles.number}>Serial Number: {serialNumber}</p>
+                </div>
+                <div className={styles.priceWrapper}>
+                  <p className={styles.price}>Total Price:
+                    <span> {totalPrice} UAH
+                    </span>
+                  </p>
+                  <p className={styles.priceSmall}>Price:
+                    <span>
+                      {price.map((p) => ` ${p.value}${p.symbol}`)}
+                    </span>
+                  </p>
+                </div>
+                <div className={styles.guaranteeWrapper}>
+                  <p className={styles.guarantee}>Guarantee Start: {guarantee.start}</p>
+                  <p className={styles.guarantee}>Guarantee End: {guarantee.end}</p>
+                </div>
+                <Image
+                  className={styles.delete}
+                  src={deleteIcon}
+                  alt="Delete icon"
+                  onClick={() => {
+                    getCurrentElement(id);
+                    openPopUp();
+                  }}
+                />
+              </div>
+            )
+          })}
+        </>
+      ) : (
+        <>
+          {prodList.map((prod: Product) => {
+            const {
+              id,
+              isNew,
+              title,
+              type,
+              specification,
+              guarantee,
+              serialNumber,
+              price
+            } = prod;
+
+            return (
+              <div key={id} className={styles.productBox}>
+                <span className={
+                  isNew ? [styles.statusBePresent] : [styles.statusAbsent]
+                } />
+                <Image
+                  className={styles.photo}
+                  src={photoProdIcon}
+                  alt="Photo product icon"
+                />
+                <div className={styles.typeWrapper}>
+                  <p className={styles.type}>{type}</p>
+                  <h6 className={styles.title}>{title}</h6>
+                </div>
+                <div className={styles.descWrapper}>
+                  <p className={styles.descSpec}>{specification}</p>
+                  <p className={styles.descGuar}>Guarantee: {guarantee.end}</p>
+                </div>
+                <div className={styles.priceWrapper}>
+                  <p className={styles.price}>Price:
+                    <span>
+                      {price.map((p) => p.isDefault ? (` ${p.value} ${p.symbol}`) : '')}
+                    </span>
+                  </p>
+                  <p className={styles.number}>Serial Number: {serialNumber}</p>
+                </div>
+                <Image
+                  className={styles.delete}
+                  src={deleteIcon}
+                  alt="Delete icon"
+                  onClick={() => {
+                    getCurrentElement(id);
+                    openPopUp();
+                  }}
+                />
+              </div>
+            )
+          })}
+        </>
+      )}
       {popUp && (
         <DeletePopUp
           setPopUpClose={pullDataPopUp}
